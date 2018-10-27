1cd88c6b9c0c84c900cbf935751d0da4aa725d4d
samkg <samyak.k.gupta@gmail.com>
2018-10-27 00:04:08 -0400

    Fixed

---
+++ include/Quaternion.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Seth Karten on 10/26/18.
+//
+#ifndef SAMPLING_QUATERNION_H
+#define SAMPLING_QUATERNION_H
+class Quaternion {
+public:
+    double W, X, Y, Z;
+    Quaternion(double w, double x, double y, double z);
+    double distance(Quaternion a, Quaternion b);
+    bool interpolate(Quaternion a, Quaternion b);
+    PQP_REAL to_rotation_matrix(Quaternion a);
+    Quaternion uniform_sample(int max);
+private:
+    double operator*(Quaternion a, Quaternion b);
+    Quaternion normalize(Quaternion a);
+    double magnitude(Quaternion a);
+}
+#endif //SAMPLING_QUATERNION_H
---
+++ src/Roadmaps.cpp
@@ -0,0 +1,5 @@
+#include "../include/Roadmaps.hpp"
+class PRM {
+};
+class RRT {
+};
